a1dc0e5aecba91638162047d036aea9fc70946e6
Aj-Sujit <sujit@ajency.in>
2021-02-10 14:06:13 +0530

    epay later init with hardcoded data

--- android/src/main/java/com/epaylater/RNEpayLaterModule.java
+++ android/src/main/java/com/epaylater/RNEpayLaterModule.java
@@ -10,6 +10,8 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 
+import org.json.JSONException;
+
 public class RNEpayLaterModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
@@ -27,7 +29,24 @@ public class RNEpayLaterModule extends ReactContextBaseJavaModule {
    @ReactMethod
     public void init(ReadableMap params) {
         if (reactContext.getCurrentActivity() != null) {
-            
+            try {
+                val userProfileBean = IDFCInitProfileModel.Builder.newInstance()
+                    .setCustPhoneNo("7666763027")
+                    .setCustFirstName("Sujit")
+                    .setCustLastName("Gawas")
+                    .setCustEmail("sujit@ajency.in")
+                    .setFCMToken("adkdkdjaskdjhaksdhhsadjkasdad")
+                    .setLatitude(4533.44)
+                    .setLongitude(1234.4)
+                    .setAccuracy(100.0)
+                    .build()
+
+                EPLManager.startIntegration(this,userProfileBean)
+            }
+            catch(error) {
+                error.printStackTrace();
+            }
+
         } else {
             throw new NullPointerException("ReactContext cannot run with Activity null");
         }
