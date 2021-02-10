3eafb051dc0d0c31a52ad6ab825fc535501ac54d
Aj-Sujit <sujit@ajency.in>
2021-02-10 18:51:13 +0530

    pass params to the sdk

--- android/src/main/java/com/epaylater/RNEpayLaterModule.java
+++ android/src/main/java/com/epaylater/RNEpayLaterModule.java
@@ -12,6 +12,9 @@ import com.facebook.react.bridge.ReadableMapKeySetIterator;
 
 import org.json.JSONException;
 
+import in.epaylater.android.nonbankingsdk.models.IDFCInitProfileModel;
+import in.epaylater.android.nonbankingsdk.managers.EPLManager;
+
 public class RNEpayLaterModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
@@ -29,24 +32,19 @@ public class RNEpayLaterModule extends ReactContextBaseJavaModule {
    @ReactMethod
     public void init(ReadableMap params) {
         if (reactContext.getCurrentActivity() != null) {
-            try {
-                val userProfileBean = IDFCInitProfileModel.Builder.newInstance()
-                    .setCustPhoneNo("7666763027")
-                    .setCustFirstName("Sujit")
-                    .setCustLastName("Gawas")
-                    .setCustEmail("sujit@ajency.in")
-                    .setFCMToken("adkdkdjaskdjhaksdhhsadjkasdad")
-                    .setLatitude(4533.44)
-                    .setLongitude(1234.4)
-                    .setAccuracy(100.0)
-                    .build()
 
-                EPLManager.startIntegration(this,userProfileBean)
-            }
-            catch(error) {
-                error.printStackTrace();
-            }
+            IDFCInitProfileModel userProfileBean = IDFCInitProfileModel.Builder.newInstance()
+                .setCustPhoneNo(params.getString("phone"))
+                .setCustFirstName(params.getString("firstName"))
+                .setCustLastName(params.getString("lastName"))
+                .setCustEmail(params.getString("email"))
+                .setFCMToken(params.getString("fcmToken"))
+                .setLatitude(params.getDouble("lat"))
+                .setLongitude(params.getDouble("log"))
+                .setAccuracy(params.getDouble("accuracy"))
+                .build();
 
+            EPLManager.INSTANCE.startIntegration(reactContext.getCurrentActivity(),userProfileBean);
         } else {
             throw new NullPointerException("ReactContext cannot run with Activity null");
         }
